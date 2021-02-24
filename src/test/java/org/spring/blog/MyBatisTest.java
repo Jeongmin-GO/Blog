d055b12b4908f796e7832f0eb2930c18b74994b0
Jeongmin-GO <go.jminn@gmail.com>
2021-02-24 13:57:13 +0900

    spring/MyBatis/MySQL 연동

---
+++ src/test/java/org/spring/blog/MyBatisTest.java
@@ -0,0 +1,32 @@
+package org.spring.blog;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:spring/dataSource-context.xml"})
+public class MyBatisTest {
+	@Inject
+    private SqlSessionFactory sqlFactory;
+    
+    @Test
+    public void testFactory(){
+        System.out.println("\n >>>>>>>>>> sqlFactory 출력 : "+sqlFactory);
+    }
+    
+    @Test
+    public void testSession() throws Exception{
+    	SqlSession session = sqlFactory.openSession();
+        try{
+            System.out.println(" >>>>>>>>>> session 출력 : "+session+"\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
