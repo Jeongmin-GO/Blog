2878e807b234bde95180e676df840449063df63f
Jeongmin-GO <go.jminn@gmail.com>
2021-03-03 11:17:04 +0900

    트랜잭션 처리(조회수증가방지)

--- src/main/java/org/spring/blog/board/service/BoardServiceImpl.java
+++ src/main/java/org/spring/blog/board/service/BoardServiceImpl.java
@@ -9,6 +9,7 @@ import org.spring.blog.board.dao.BoardDAO;
 import org.spring.blog.board.vo.BoardVO;
 import org.spring.blog.error.controller.NotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BoardServiceImpl implements BoardService{
@@ -24,21 +25,23 @@ public class BoardServiceImpl implements BoardService{
 	public void insertBoard(BoardVO vo) throws Exception{
 		boardDAO.insertBoard(vo);
 	}
-
+	
+	@Transactional
 	@Override
 	public BoardVO selectBoardDetail(int bno) throws Exception {
 		BoardVO boardVO = new BoardVO();
 		boardDAO.updateViewCnt(bno);
-//		boardVO = boardDAO.selectBoardDetail(bno);
+		boardVO = boardDAO.selectBoardDetail(bno);
 		
+		//예외처리 test
 		/*category 저장크기보다 큰 문자열을 저장하도록 셋팅 후 게시물 수정 로직 호출하면 수정 sql문 처리시 문제가 발생*/
-		try {
-			boardVO.setBno(bno);
-			boardVO.setCategory("111111111111111111111111111111111111");
-			boardDAO.updateBoard(boardVO);
-		}catch (RuntimeException e) {
-			throw new NotFoundException();
-		}
+//		try {
+//			boardVO.setBno(bno);
+//			boardVO.setCategory("111111111111111111111111111111111111");
+//			boardDAO.updateBoard(boardVO);
+//		}catch (RuntimeException e) {
+//			throw new NotFoundException();
+//		}
 //		return boardDAO.selectBoardDetail(bno);
 		return boardVO;
 	}
